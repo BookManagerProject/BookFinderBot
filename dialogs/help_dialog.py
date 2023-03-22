e970572c97a3791ff3fb1724765945b938d855d7
vinciraia99 <99raia99@gmail.com>
2023-03-22 18:07:06 +0100

    restore help functionality

--- dialogs/help_dialog.py
+++ dialogs/help_dialog.py
@@ -1,10 +1,7 @@
-from botbuilder.core import MessageFactory, UserState
-from botbuilder.dialogs import (
-    PromptOptions
-)
+from botbuilder.core import MessageFactory, UserState, CardFactory
 from botbuilder.dialogs import WaterfallDialog, WaterfallStepContext, DialogTurnResult
 from botbuilder.dialogs.prompts import TextPrompt
-from botbuilder.schema import InputHints, HeroCard, CardAction, ActionTypes
+from botbuilder.schema import HeroCard, CardAction, ActionTypes
 
 from dialogs import CancelAndHelpDialog
 
@@ -26,20 +23,7 @@ class HelpDialog(CancelAndHelpDialog):
         self.initial_dialog_id = WaterfallDialog.__name__
 
     async def help_step(self, step_context: WaterfallStepContext) -> DialogTurnResult:
-        message_text = 'Ecco le mie principali funzioni:\n\n'
-        message_text += '  1. Cerca un libro.\n\n' \
-                        '  2. Effetture il login per gestire i libri preferiti.\n\n' \
-                        '  3. Effettuare la regitrazione.\n\n' \
-                        '  4. Visulizza preferiti.\n\n' \
-                        '  5. Elimina preferiti.\n\n'
-        prompt_message = MessageFactory.text(
-            message_text, message_text, InputHints.ignoring_input
-        )
-        await step_context.prompt(
-            TextPrompt.__name__, PromptOptions(prompt=prompt_message)
-        )
-
-        card = HeroCard(title='Per comodità clicca la funzionalità che desideri', buttons=[
+        card = HeroCard(title='Ecco le mie principali funzioni:', buttons=[
             CardAction(
                 title='Cerca un libro',
                 type=ActionTypes.im_back,
@@ -78,6 +62,6 @@ class HelpDialog(CancelAndHelpDialog):
         ],
                         )
 
-        # message = MessageFactory.attachment(CardFactory.hero_card(card))
-        # await step_context.context.send_activity(message)
+        message = MessageFactory.attachment(CardFactory.hero_card(card))
+        await step_context.context.send_activity(message)
         return await step_context.end_dialog(step_context.options)
